cddb9fa401cd85bff4bb4be9ec70ae8b4eeea763
nelsnew07-cell <nelsnew07@gmail.com>
2026-06-26 17:58:30 +0800

    Remove claimedTickets Map from index.js
    Removed unused claimedTickets Map declaration.

--- index.js
+++ index.js
@@ -89,10 +89,6 @@ function saveLeaderboard() {
 
 }
 
-/* ================= CLAIM CACHE ================= */
-
-const claimedTickets = new Map();
-
 /* ================= STAFF ROLES ================= */
 
 const staffRoles = [
